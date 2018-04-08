cea959b8a3e5a4bcc010c76b96142399b5e7a74c
xingtanzjr <xingtanzjr@163.com>
2018-04-08 20:36:22 +0800

    xx

--- src/main/java/com/thulab/read/OpenTSDBReader.java
+++ src/main/java/com/thulab/read/OpenTSDBReader.java
@@ -1,14 +1,15 @@
 package com.thulab.read;
 
 import com.thulab.data.template.DatabaseConfig;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.sql.SQLException;
 
 /**
@@ -17,37 +18,22 @@ import java.sql.SQLException;
 public class OpenTSDBReader extends DBReader {
 
     private String url;
+    private HttpClient client;
 
     private OpenTSDBReader(DatabaseConfig dbconfig) {
         url = "http://" + dbconfig.getUrl() + "/api/query?";
+        client = HttpClients.createDefault();
     }
 
     @Override
     protected void executeQuery(String sql) throws SQLException {
-        String[] args = sql.split(",");
-        String start = args[0];
-        String end = args[1];
-        String m = args[2];
-        StringBuilder param = new StringBuilder("start=").append(start).append("&");
-        if (!end.equals("")) {
-            param.append("end=").append(end).append("&");
-        }
-        param.append("m=").append(m);
+        HttpGet get = new HttpGet(String.format("%s%s", url, sql));
         try {
-            URL url = null;
-            url = new URL(String.format("%s%s", url, param));
-            URLConnection connection = url.openConnection();
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"));
-            String line;
-            StringBuilder sb = new StringBuilder();
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-            br.close();
-            System.out.println(sb.toString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
+            HttpResponse response = client.execute(get);
+            HttpEntity entity = response.getEntity();
+            String result = EntityUtils.toString(entity, "UTF-8");
+//            System.out.println(result);
+        } catch (ClientProtocolException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
---
+++ src/test/java/com/thulab/read/OpenTSDBReaderTest.java
@@ -0,0 +1,20 @@
+package com.thulab.read;
+
+import com.thulab.data.template.DatabaseConfig;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+/**
+ * Created by zhangjinrui on 2018/4/8.
+ */
+public class OpenTSDBReaderTest {
+
+    @Test
+    public void test() throws SQLException {
+        DatabaseConfig dbconfig = new DatabaseConfig();
+        dbconfig.setUrl("192.168.3.91:4242");
+        DBReader dbReader = OpenTSDBReader.getConn(dbconfig);
+        System.out.println(dbReader.query("start=1522028100000&m=sum:100s-count:dev_a.s_ai"));
+    }
+}
